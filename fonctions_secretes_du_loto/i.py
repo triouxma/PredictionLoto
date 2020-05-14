180a30ddbbef4dadaba7d116637a392030599c00
Tia <tia@Tias-MacBook-Air.local>
2020-05-14 15:01:20 +0200

    fonction i à vérifier

--- fonctions_secretes_du_loto/i.py
+++ fonctions_secretes_du_loto/i.py
@@ -1,4 +1,7 @@
 # - si le paramètre texte possède 9 caractères : ajouter le caractère '0' en début de chaîne et renvoyer le résultat
 # - sinon renvoyer le texte tel quel
 def i(texte):
-    # TODO
+    mot = texte
+    if len(texte) ==9:
+        mot = '0' + mot
+    return mot
