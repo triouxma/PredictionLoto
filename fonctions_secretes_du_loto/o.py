1bdad3d178edfcfe39772bb9465cedd88d089745
Bloup Bloup <corentin.lcrq@gmail.com>
2020-05-14 14:54:06 +0200

    converti chaque élément de la liste en string

--- fonctions_secretes_du_loto/o.py
+++ fonctions_secretes_du_loto/o.py
@@ -1,3 +1,10 @@
 # Convertir chaque élément du paramètre liste en chaînes de caractères et renvoyer la liste modifiée
 def o(liste):
-    # TODO
+    try :
+        list_to_list = list()
+        for i in liste:
+            list_to_list.append(str(i))
+        return list_to_list
+    except :
+        print("Problème avec la liste")
+
