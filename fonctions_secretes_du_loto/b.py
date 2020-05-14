20e8bc204cd33133c4dc43e9edd3f64f1f74e140
Bloup Bloup <corentin.lcrq@gmail.com>
2020-05-14 14:26:22 +0200

    converti le texte en nombre

--- fonctions_secretes_du_loto/b.py
+++ fonctions_secretes_du_loto/b.py
@@ -1,3 +1,7 @@
 # Convertit l'argument texte en nombre entier et renvoie ce nombre
 def b(texte):
-    # TODO
+    try:
+        text_to_int = int(texte)
+        return text_to_int
+    except :
+        print("Ce n'est pas un nombre ! ")
